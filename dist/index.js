2b6d06cff182a1abc131d50e9b09b4b803c5a294
kkepinska <katarzyna.kpinska@student.uj.edu.pl>
2023-04-02 11:48:58 +0200

    chat app

--- dist/index.js
+++ dist/index.js
@@ -8,24 +8,42 @@ const http_1 = __importDefault(require("http"));
 const socket_io_1 = require("socket.io");
 const app = (0, express_1.default)();
 const server = http_1.default.createServer(app);
-const io = new socket_io_1.Server(server);
-const port = 8080; // default port to listen
-// define a route handler for the default home page
-app.get('/', (req, res) => {
-    res.sendFile('/home/kasia/Documents/PP2/Server_Hanabi/src' + '/index.html');
+const io = new socket_io_1.Server(server, {
+    cors: { origin: '*' }
 });
+const port = 8080; // default port to listen
 io.on('connection', (socket) => {
+    // tslint:disable-next-line:no-console
+    console.log('a user connected');
+    socket.on('message', (message) => {
+        // tslint:disable-next-line:no-console
+        console.log(message);
+        io.emit('message', `${socket.id.substr(0, 2)} said ${message}`);
+    });
+    socket.on('disconnect', () => {
+        // tslint:disable-next-line:no-console
+        console.log('a user disconnected!');
+    });
+});
+server.listen(port, () => {
+    // tslint:disable-next-line:no-console
+    console.log(`listening on port ${port}`);
+});
+/*
+// define a route handler for the default home page
+
+  io.on('connection', (socket:Socket) => {
     socket.on("join", (arg, callback) => {
         // tslint:disable-next-line:no-console
         console.log(arg);
         callback("got it");
         socket.join("some room");
         io.to("some room").emit("some event");
-    });
-});
+      });
+  });
 // start the Express server
-app.listen(port, () => {
+app.listen( port, () => {
     // tslint:disable-next-line:no-console
-    console.log(`server started at http://localhost:${port}`);
-});
+} );
+*/ 
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,70 @@ import { Socket, Server } from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {origin : '*'}
+});
 
 const port = 8080; // default port to listen
 
+
+io.on('connection', (socket) => {
+        // tslint:disable-next-line:no-console
+        console.log('a user connected');
+
+  socket.on('message', (message) => {
+        // tslint:disable-next-line:no-console
+        console.log(message);
+    io.emit('message', `${socket.id.substr(0, 2)} said ${message}`);
+  });
+
+  socket.on('disconnect', () => {
+        // tslint:disable-next-line:no-console
+        console.log('a user disconnected!');
+  });
+});
+
+server.listen(port, () => {
+ // tslint:disable-next-line:no-console
+  console.log(`listening on port ${port}`)
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 // define a route handler for the default home page
 
   io.on('connection', (socket:Socket) => {
@@ -22,5 +82,5 @@ const port = 8080; // default port to listen
 // start the Express server
 app.listen( port, () => {
     // tslint:disable-next-line:no-console
-    console.log( `server started at http://localhost:${ port }` );
 } );
+*/
